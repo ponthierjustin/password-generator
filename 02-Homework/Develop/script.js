a4a9407199e3cb260f048fd58a30d989f87c1755
Justin <ponthierjustin@gmail.com>
2020-06-08 16:35:47 -0500

    added variables to writePassword function

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -3,12 +3,21 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
+  var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  var stringLen = 8;
+  var random = '';
+  for (var i=0; i<string_length; i++) {
+    var rnum = Math.floor(Math.random() * chars.length);
+    random += chars.substring(rnum,rnum+1);
+  }
+  
+}
+  var passwordText = document.querySelector("#password");{
   passwordText.value = password;
 
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
