ccca73f449ac3381e382c2f2cb93ec2c95c5b77e
Justin <ponthierjustin@gmail.com>
2020-06-08 19:27:10 -0500

    added variables

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -1,4 +1,5 @@
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
 function writePassword() {
@@ -10,17 +11,40 @@ function writePassword() {
     writePassword();
   }
   function writePassword() {
-    var uppers = confirm("Would you like uppercase letters in your password?");
-    var lowers = confirm("Would you like lower case numbers in your password?");
-    var numbers = confirm("Would you like numbers in your password?");
+    var up = confirm("Would you like uppercase letters in your password?");
+    var low = confirm("Would you like lower case numbers in your password?");
+    var num = confirm("Would you like numbers in your password?");
     var special = confirm(
       "Would you like special characters in your password?"
     );
     var uChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var lChar = "abcdefghijklmnopqrstuvwxyz";
     var int = "1, 2, 3, 4, 5, 6, 7, 8, 9, 0";
-    var spe = ["!#$%^&*=+"];
-    var userChoices = [];
+    var spe = "!#$%^&*=+"
+    var userChoices = '';
+    if (up) {
+      userChoice = userChoices.push(uChar);
+    }
+    if (low) {
+      userChoice = userChoices.push(lChar);
+    }
+    if (num) {
+      userChoice = userChoices.push(int);
+    }
+    if (special) {
+      userChoice = userChoices.push(spe);
+    } else {
+      alert("Enter valid requirements for password.");
+      writePassword();
+    }
+    console.log(userChoices);
+    var password = "";
+    for (var i = 0; i < passwordLength; i++) {
+      var randomPasswordNum = Math.floor(Math.random() * userChoices.length);
+      console.log(randomPasswordNum);
+      password += userChoices[randomPasswordNum];
+    }
+  }
 
   var password = writePassword();
 
