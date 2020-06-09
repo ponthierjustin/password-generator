a5a72c490c5b628d6e36cdc80216d42dfadacd1b
Justin <ponthierjustin@gmail.com>
2020-06-08 19:17:07 -0500

    added variables to code

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -1,23 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
 function writePassword() {
-  var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  var stringLen = 8;
-  var random = '';
-  for (var i=0; i<string_length; i++) {
-    var rnum = Math.floor(Math.random() * chars.length);
-    random += chars.substring(rnum,rnum+1);
+  var passwordLength = prompt(
+    "Please enter desired password length between 8 and 128 characters"
+  );
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Please enter a password between 8 and 128 characters");
+    writePassword();
   }
-  
-}
-  var passwordText = document.querySelector("#password");{
+  function writePassword() {
+    var uppers = confirm("Would you like uppercase letters in your password?");
+    var lowers = confirm("Would you like lower case numbers in your password?");
+    var numbers = confirm("Would you like numbers in your password?");
+    var special = confirm(
+      "Would you like special characters in your password?"
+    );
+    var uChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lChar = "abcdefghijklmnopqrstuvwxyz";
+    var int = "1, 2, 3, 4, 5, 6, 7, 8, 9, 0";
+    var spe = ["!#$%^&*=+"];
+    var userChoices = [];
+
+  var password = writePassword();
+
+  var passwordText = document.querySelector("#password");
+
   passwordText.value = password;
-
 }
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
